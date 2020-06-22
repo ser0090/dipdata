a57bd0e1c0c4987dd64fe0aa6da8bf0a20fa9c5d
sergio <ser.0090@gmail.com>
2020-06-22 20:34:58 -0300

    analisis y curacion init

---
+++ curacion/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Set-up
+-----------------
+"""
+
+from distutils.core import setup
+
+
+setup(
+    name='curacion',
+    version='1.0',
+    description='Analisis y visualizacion de datos',
+    author='Sergio Sulca',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'jupyterlab',
+        'ftfy<5.6'
+    ]
+)
