4a87546b0e5e2aed70d42f94ac6cfc04b6955a12
sergio <ser.0090@gmail.com>
2020-08-29 14:38:05 -0300

    setup env

---
+++ supervised/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Set-up
+-----------------
+"""
+
+from distutils.core import setup
+
+
+setup(
+    name='introml',
+    version='1.0',
+    description='intreduccion a machine learning',
+    author='Sergio Sulca',
+    install_requires=[
+        'jupyterlab',
+        'numpy',
+        'matplotlib',
+        'scikit-learn',
+        'pandas',
+        'seaborn',
+        'scipy',
+        'Keras'
+    ]
+)
