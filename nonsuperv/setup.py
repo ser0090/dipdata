09a7ab8e96ee1247aa1a8b98a1da1672502a1c8e
sergio <ser.0090@gmail.com>
2020-09-11 22:17:46 -0300

    libs for nonsup

---
+++ nonsuperv/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Set-up
+-----------------
+"""
+
+from distutils.core import setup
+
+
+setup(
+    name='nonsuperv',
+    version='1.0',
+    description='intreduccion a machine learning',
+    author='Sergio Sulca',
+    install_requires=[
+        'jupyterlab',
+        'notebook',
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'seaborn',
+        'scipy',
+        'nodejs',
+        'ipywidgets',
+        'widgetsnbextension',
+        'plotly',
+    ]
+)
